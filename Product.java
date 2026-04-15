1d985bcfd97a288eac45e8cd576068ae17a67dd2
Javier Morales <javier.morales@codifika.com.mx>
2026-04-15 12:22:36 -0600

    added Product class

---
+++ Product.java
@@ -0,0 +1,11 @@
+public class Product {
+	private String id;
+	private String name;
+	private float price;
+
+       public Product(String id, String name, float price) {
+       		this.id = id;
+	 	this.name = name;
+		this.price = price;
+       }
+}
